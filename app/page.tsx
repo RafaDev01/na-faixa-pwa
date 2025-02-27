6bd7094c18d812d88cd3d967b750646b64552914
Rafael do Nascimento Santos <rflwwe123@hotmail.com>
2025-02-26 23:47:01 -0300

    adicionado função no page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,22 @@
+'use client'
+
 import styles from "./page.module.css";
+
+import { useState, useEffect } from 'react'
+import { subscribeUser, unsubscribeUser, sendNotification } from './actions'
+
+function urlBase64ToUint8Array(base64String: string) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
+
+  const rawData = window.atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
+}
 
 export default function Home() {
   return (
